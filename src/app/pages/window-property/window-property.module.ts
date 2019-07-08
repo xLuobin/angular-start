b4996768a79ea4791abd0a30d5365b859164fdcf
bin.luo <bin.luo@CNbin.luo>
2019-07-08 18:05:47 +0800

    feat: add window-property module and entry component

---
+++ src/app/pages/window-property/window-property.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { DetailComponent } from './detail/detail.component';
+
+@NgModule({
+    declarations: [
+        DetailComponent
+    ],
+    imports: [
+    ],
+    providers: []
+})
+export class WindowPropertyModule { }
