52b6b132a995124c7008f04a612390ac9e8da840
bin.luo <bin.luo@CNbin.luo>
2019-07-08 14:47:34 +0800

    refactor: use angular-cli to build app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-/**
- * Created by X.Jason on 2017/3/23.
- */
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h2>{{name}}</h2>`
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = '我就是屌得一逼的Angular, 怎么不服，打我啊！'
+  title = 'Angular-start';
 }
---
+++ src/environments/environment.e2e.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  e2e: true
+};
